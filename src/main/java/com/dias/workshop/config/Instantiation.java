a31f90b45921aefc1f745e36268816bca9d468d2
Matheus Dias <matheus.dias@ifood.com.br>
2020-04-20 22:38:43 -0300

    Add config

---
+++ src/main/java/com/dias/workshop/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.dias.workshop.config;
+
+import com.dias.workshop.domain.User;
+import com.dias.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
